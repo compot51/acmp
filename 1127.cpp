c57690604e715bdcadb6e1152ed5b828ecdbbdc4
Yakov <72313050+compot51@users.noreply.github.com>
2022-10-12 12:57:46 +0300

    Create 1127.cpp

---
+++ 1127.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+ 
+int main() {  
+    int n;
+    cin >> n;
+    int pow2 = 1;
+    while (pow2 <= n)
+    {
+        cout << pow2 <<' ';
+        pow2 *=2;
+    }
+}
